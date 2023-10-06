a1b6527c8296ab3719189e470c9548c943cf0429
meryemkaftar <merykft@gmail.com>
2023-10-06 16:41:57 +0300

    Adding my project

---
+++ app.js
@@ -0,0 +1,42 @@
+function getRandomValue(min, max) {
+  // place the min and max as arguments in the Vue methods
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+const app = Vue.createApp({
+  data() {
+    return {
+      monsterHealth: 100,
+      playerHealth: 100,
+    };
+  },
+
+  methods: {
+    attackMonster() {
+      const attackValue = getRandomValue(5, 12);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      const attackValue = getRandomValue(8, 15);
+      this.playerHealth -= attackValue;
+    },
+
+    healPlayer() {
+      const counter = 0;
+      this.playerHealth += getRandomValue(3, 10);
+      counter = (counter + 1) % 60;
+    },
+
+    specialAttack() {
+      const attackValue = getRandomValue(8, 15);
+      this.monsterHealth -= attackValue;
+    },
+
+    surrender() {
+      this.playerHealth = 0;
+    },
+  },
+});
+
+app.mount("#game");
